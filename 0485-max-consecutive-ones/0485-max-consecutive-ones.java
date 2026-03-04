7231ab6549fa292be7d4a0bc5f2cd0c56abcfdcb
VARshata05 <varshatabalagar@gmail.com>
2026-03-04 23:07:04 +0530

    Time: 3 ms (61.02%), Space: 52.7 MB (24.63%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.java
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -3,13 +3,13 @@ class Solution {
         int count=0;
         int max=0;
         for(int i=0;i<nums.length;i++){
-            if(nums[i]==0){
-                count=0;
-            } 
-            else{  
+            if(nums[i]==1){
                 count++;
-                max=Math.max(max,count); 
+                max=Math.max(max,count);
             }
+            else{
+                count=0;
+            }  
         }
         return max;
     }
