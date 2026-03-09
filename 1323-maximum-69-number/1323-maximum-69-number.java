565bdf27b7e425a0a4d22aeabe42d634e0ee4edb
VARshata05 <varshatabalagar@gmail.com>
2026-03-09 19:17:04 +0530

    Time: 0 ms (100%), Space: 42.1 MB (90.26%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.java
+++ 1323-maximum-69-number/1323-maximum-69-number.java
@@ -1,7 +1,30 @@
+// class Solution {
+//     public int maximum69Number (int num) {
+//         String s = String.valueOf(num);
+//         s=s.replaceFirst("6","9");
+//         return Integer.parseInt(s);
+//     }
+// }
 class Solution {
     public int maximum69Number (int num) {
-        String s = String.valueOf(num);
-        s=s.replaceFirst("6","9");
-        return Integer.parseInt(s);
+        int temp = num;
+        int position = 0;
+        int sixPosition = -1;
+
+        // Find the leftmost 6 (actually last 6 when scanning right to left)
+        while (temp > 0) {
+            if (temp % 10 == 6) {
+                sixPosition = position;
+            }
+            temp = temp / 10;
+            position++;
+        }
+
+        // Change that 6 to 9
+        if (sixPosition != -1) {
+            num += 3 * Math.pow(10, sixPosition);
+        }
+
+        return num;
     }
 }
