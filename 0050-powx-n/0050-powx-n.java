c27597b930e8c7a0f1f64b1521207570da2a62da
VARshata05 <varshatabalagar@gmail.com>
2026-03-11 22:15:26 +0530

    Time: 0 ms (100%), Space: 47.9 MB (70.96%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,18 @@
+class Solution {
+    public double myPow(double x, int n) {
+        double res=1;
+        long N=n;
+        if(N<0){
+            x=1/x;
+            N=-N;
+        }
+        while(N>0){
+            if(N%2 == 1){
+                res=res*x;
+            }
+            x=x*x;
+            N/=2;
+        }
+        return res;
+    }
+}
