e8b9faf3d51d98f0e101f432b0b01676eab16ee8
VARshata05 <varshatabalagar@gmail.com>
2026-04-15 09:07:34 +0530

    Time: 0 ms (100%), Space: 41.9 MB (76.64%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -3,11 +3,11 @@ class Solution {
         if(n==1) return 1;
         if(n==2) return 2;
         int[] a=new int[n];
-        a[0]=1;
+        a[0] =1;
         a[1]=2;
         for(int i=2;i<n;i++){
             a[i]=a[i-1]+a[i-2];
         }
-        return a[n-1];  
+        return a[n-1];
     }
 }
