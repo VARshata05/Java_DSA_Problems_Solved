87c7e359a6b7597927262783c51b6e6c823754ba
VARshata05 <varshatabalagar@gmail.com>
2026-06-15 14:24:47 +0530

    Time: 0 ms (100%), Space: 52.8 MB (75.93%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -9,26 +9,26 @@ class Solution {
         // return nums[nums.length - 1];
 
         //bit manipulation
-        int result=0;
-        for(int num:nums){
-            result^=num;
-        }
-        return result;  
+        // int result=0;
+        // for(int num:nums){
+        //     result^=num;
+        // }
+        // return result;  
 
         //Binary Search
-        // int left = 0;
-        // int right = nums.length - 1;
-        // while (left < right) {
-        //     int mid = left + (right - left) / 2;
-        //     if (mid % 2 == 1) {
-        //         mid--;
-        //     }
-        //     if (nums[mid] == nums[mid + 1]) {
-        //         left = mid + 2;
-        //     } else {
-        //         right = mid;
-        //     }
-        // }
-        // return nums[left];      
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (mid % 2 == 1) {
+                mid--;
+            }
+            if (nums[mid] == nums[mid + 1]) {
+                left = mid + 2;
+            } else {
+                right = mid;
+            }
+        }
+        return nums[left];      
     }
 }
